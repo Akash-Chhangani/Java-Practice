4992e8324e8232be61f4535f3c731ca11fd76cec
Akash Chhangani <156087593+Akash-Chhangani@users.noreply.github.com>
2024-07-31 23:22:34 +0530

    HollowTriangle Pattern

--- Patterns/Basic Patterns/HollowTriangle.java
+++ Patterns/Basic Patterns/HollowTriangle.java
@@ -1,21 +1,18 @@
 // Java Program to print 
 // Hollow triangle pattern
-import java.util.*;
-
-public class GeeksForGeeks {
+public class HollowTriangle {
     // Function to demonstrate pattern
-    public static void printPattern(int n)
-    {
+    public static void printPattern(int n) {
         int i, j, k;
 
         // outer loop to handle rows
         for (i = 1; i <= n; i++) {
-          
+
             // inner loop to print spaces.
             for (j = i; j < n; j++) {
                 System.out.print(" ");
             }
-          
+
             for (k = 1; k <= (2 * i - 1); k++) {
                 // printing stars.
                 if (k == 1 || i == n || k == (2 * i - 1)) {
@@ -32,8 +29,7 @@ public class GeeksForGeeks {
     }
 
     // Driver Function
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
         int n = 6;
         printPattern(n);
     }
