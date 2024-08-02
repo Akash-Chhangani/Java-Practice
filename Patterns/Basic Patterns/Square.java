2ba006c2feb4ec3be1d2bfe4095320831fb4dc23
Akash Chhangani <156087593+Akash-Chhangani@users.noreply.github.com>
2024-08-02 22:57:15 +0530

    Square Pattern

--- Patterns/Basic Patterns/Square.java
+++ Patterns/Basic Patterns/Square.java
@@ -1,12 +1,9 @@
 // Java Program to print
 // Square fill pattern
-import java.util.*;
-
-public class GeeksForGeeks {
+public class Square {
 
     // Function to demonstrate pattern
-    public static void printPattern(int n)
-    {
+    public static void printPattern(int n) {
         int i, j;
 
         // outer loop to handle rows
@@ -23,8 +20,7 @@ public class GeeksForGeeks {
     }
 
     // Driver Function
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
         int n = 6;
         printPattern(n);
     }
