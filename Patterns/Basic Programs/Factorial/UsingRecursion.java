8a86d727f9ff082781be503178016da4eb99e751
Akash Chhangani <156087593+Akash-Chhangani@users.noreply.github.com>
2024-08-02 23:02:08 +0530

    UsingRecursion Pattern

--- Patterns/Basic Programs/Factorial/UsingRecursion.java
+++ Patterns/Basic Programs/Factorial/UsingRecursion.java
@@ -7,7 +7,7 @@ class UsingRecursion {
   }
 
   public static void main(String args[]) {
-    int i, fact = 1;
+    int fact = 1;
     int number = 4;// It is the number to calculate factorial
     fact = factorial(number);
     System.out.println("Factorial of " + number + " is: " + fact);
